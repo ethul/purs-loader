466c00689bf6e411becec2c1b6d5d3246f4aa834
eric <thul.eric@gmail.com>
2017-08-12 10:47:55 -0400

    Adding source map support (#102)
    * Adding source map support
    
    Resolves #101
    
    * Remove unused fs

--- src/index.js
+++ src/index.js
@@ -22,6 +22,8 @@ const ide = require('./ide');
 
 const toJavaScript = require('./to-javascript');
 
+const sourceMaps = require('./source-maps');
+
 const dargs = require('./dargs');
 
 const spawn = require('cross-spawn').sync
@@ -168,9 +170,11 @@ module.exports = function purescriptLoader(source, map) {
 
   const psModule = {
     name: psModuleName,
-    load: js => callback(null, js),
+    source: source,
+    load: ({js, map}) => callback(null, js, map),
     reject: error => callback(error),
     srcPath: this.resourcePath,
+    remainingRequest: loaderUtils.getRemainingRequest(this),
     srcDir: path.dirname(this.resourcePath),
     jsPath: path.resolve(path.join(options.output, psModuleName, 'index.js')),
     options: options,
@@ -226,6 +230,7 @@ module.exports = function purescriptLoader(source, map) {
       ide.rebuild(psModule)
       .then(() =>
         toJavaScript(psModule)
+          .then(js => sourceMaps(psModule, js))
           .then(psModule.load)
           .catch(psModule.reject)
       )
@@ -255,6 +260,7 @@ module.exports = function purescriptLoader(source, map) {
                 Promise.map(cache.deferred, psModule =>
                   ide.load(psModule)
                     .then(() => toJavaScript(psModule))
+                    .then(js => sourceMaps(psModule, js))
                     .then(psModule.load)
                 )
               )
@@ -287,6 +293,7 @@ module.exports = function purescriptLoader(source, map) {
     debugVerbose('compilation is already finished, loading module %s', psModule.name);
 
     toJavaScript(psModule)
+      .then(js => sourceMaps(psModule, js))
       .then(psModule.load)
       .catch(psModule.reject);
   }
@@ -320,6 +327,7 @@ module.exports = function purescriptLoader(source, map) {
         .then(() =>
           Promise.map(cache.deferred, psModule =>
             toJavaScript(psModule)
+              .then(js => sourceMaps(psModule, js))
               .then(psModule.load)
           )
         )
